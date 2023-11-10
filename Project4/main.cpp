a29b33451e2e17ef979c3a955fa4c43c1dc8b98c
IlliaNobleSoftware <illia.shcheboruk@noble.software>
2023-11-10 10:27:05 +0100

    Add project files.

---
+++ Project4/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <SFML/Graphics.hpp>
+
+class GameObject {
+public:
+	virtual void draw(sf::RenderWindow& window) = 0;
+};
+
+
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1280, 768), "PG Invaders");
+	
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+
+		window.display();
+	}
+	return 0;
+}
